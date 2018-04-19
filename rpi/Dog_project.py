1c38e7c019896a0e3183f40bab51385db4c207df
Jasneet Singh Sawhney <aashitsingh2010@gmail.com>
2018-04-20 04:40:12 +0530

    Completed wth all the stuff:smail:

---
+++ rpi/Dog_project.py
@@ -0,0 +1,154 @@
+from sklearn.datasets import load_files
+from keras.utils import np_utils
+import numpy as np
+from glob import glob
+import cv2
+import matplotlib.pyplot as plt
+
+# define function to load train, test, and validation datasets
+def load_dataset(path):
+    data = load_files(path)
+    dog_files = np.array(data['filenames'])
+    dog_targets = np_utils.to_categorical(np.array(data['target']), 133)
+    return dog_files, dog_targets
+
+# load train, test, and validation datasets
+train_files, train_targets = load_dataset('dogImages/train')
+valid_files, valid_targets = load_dataset('dogImages/valid')
+test_files, test_targets = load_dataset('dogImages/test')
+
+# load list of dog names
+dog_names = [item[20:-1] for item in sorted(glob("dogImages/train/*/"))]
+
+# print statistics about the dataset
+print('There are %d total dog categories.' % len(dog_names))
+print('There are %s total dog images.\n' % len(np.hstack([train_files, valid_files, test_files])))
+print('There are %d training dog images.' % len(train_files))
+print('There are %d validation dog images.' % len(valid_files))
+print('There are %d test dog images.'% len(test_files))
+
+import random
+random.seed(8675309)
+
+# load filenames in shuffled human dataset
+human_files = np.array(glob("lfw/*/*"))
+random.shuffle(human_files)
+
+# print statistics about the dataset
+print('There are %d total human images.' % len(human_files))
+
+#Detect Dogs
+from keras.applications.resnet50 import ResNet50
+
+# define ResNet50 model
+ResNet50_model = ResNet50(weights='imagenet')
+
+from keras.preprocessing import image
+from tqdm import tqdm
+
+def path_to_tensor(img_path):
+    # loads RGB image as PIL.Image.Image type
+    img = image.load_img(img_path, target_size=(224, 224))
+    # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
+    x = image.img_to_array(img)
+    # convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
+    return np.expand_dims(x, axis=0)
+
+def paths_to_tensor(img_paths):
+    list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
+    #making a stack of all the resized images
+    return np.vstack(list_of_tensors)
+
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+def ResNet50_predict_labels(img_path):
+    # returns prediction vector for image located at img_path
+    img = preprocess_input(path_to_tensor(img_path))
+    return np.argmax(ResNet50_model.predict(img))
+
+### returns "True" if a dog is detected in the image stored at img_path
+def dog_detector(img_path):
+    prediction = ResNet50_predict_labels(img_path)
+    return ((prediction <= 268) & (prediction >= 151))
+
+#No. of dogs detected
+dogs_detected = 0
+for human in human_files_short:
+    if dog_detector(human):
+        dogs_detected += 1
+
+print('Number of dogs detected in humans', dogs_detected)
+
+
+dogs_detected = 0
+for dog in dog_files_short:
+    if dog_detector(dog):
+        dogs_detected += 1
+
+print('Number of dogs detected in dogs', dogs_detected)
+
+
+#Pre-process the Data
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+
+# pre-process the data for Keras
+#scaling the images for keras
+train_tensors = paths_to_tensor(train_files).astype('float32')/255
+valid_tensors = paths_to_tensor(valid_files).astype('float32')/255
+test_tensors = paths_to_tensor(test_files).astype('float32')/255
+
+
+#Transfer Learning Model
+
+#Obtain bottleneck features
+bottleneck_features = np.load('bottleneck_features/DogVGG16Data.npz')
+train_VGG16 = bottleneck_features['train']
+valid_VGG16 = bottleneck_features['valid']
+test_VGG16 = bottleneck_features['test']
+
+VGG16_model = Sequential()
+VGG16_model.add(GlobalAveragePooling2D(input_shape=train_VGG16.shape[1:]))
+VGG16_model.add(Dense(133, activation='softmax'))
+
+print(VGG16_model.summary())
+
+#Compile the model
+VGG16_model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+checkpointer = ModelCheckpoint(filepath='saved_models/weights.best.VGG16.hdf5',
+                               verbose=1, save_best_only=True)
+
+VGG16_model.fit(train_VGG16, train_targets,
+          validation_data=(valid_VGG16, valid_targets),
+          epochs=20, batch_size=20, callbacks=[checkpointer], verbose=1)
+
+# Load the Model with the Best Validation Loss
+VGG16_model.load_weights('saved_models/weights.best.VGG16.hdf5')
+
+
+# Test the model
+#Now, we can use the CNN to test how well it identifies breed within our test dataset of dog images.
+#We print the test accuracy below.
+
+# get index of predicted dog breed for each image in test set
+VGG16_predictions = [np.argmax(VGG16_model.predict(np.expand_dims(feature, axis=0))) for feature in test_VGG16]
+
+# report test accuracy
+test_accuracy = 100*np.sum(np.array(VGG16_predictions)==np.argmax(test_targets, axis=1))/len(VGG16_predictions)
+print('Test accuracy: %.4f%%' % test_accuracy)
+
+
+#Predict Dog Breed with the model
+from extract_bottleneck_features import *
+
+def VGG16_predict_breed(img_path):
+    # extract bottleneck features
+    bottleneck_feature = extract_VGG16(path_to_tensor(img_path))
+    # obtain predicted vector
+    predicted_vector = VGG16_model.predict(bottleneck_feature)
+    # return dog breed that is predicted by the model
+    return dog_names[np.argmax(predicted_vector)]
+
+
+VGG16VGG16_predict_breed()
